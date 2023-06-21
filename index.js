3a5c064a08d967ed91acc4cecfb7d16f7cb417dd
francesco201088 <129757936+francesco201088@users.noreply.github.com>
2023-06-21 17:10:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+// Un algoritmo è una serie di istruzioni passo passo che descrivono come fare qualcosa.
+// Una variabile è il contenitore di qualcosa, per esempio una ciotola per della farina.
+// No non sono la stessa cosa, poiche undefined vuol dire che una variabile non è stata dichiarata, mentre con null cè una variabile con quel valore.
